8748eb508eddd13a3527d41c994ecfc318b25c4c
Jasmeen Kaur <bajwameen@gmail.com>
2026-05-14 14:46:43 -0400

    added code for client side

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>
+        This is a Full Stack React app for Keeping Contacts
+      </p>
+      <p className='bg-dark p'>
+        <strong>Version 1.0.0</strong>
+      </p>
+    </div>
+  );
+};
+export default About;
